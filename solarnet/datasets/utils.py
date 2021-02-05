Fix mistake in image normalization. Resolves #10

solarnet/datasets/utils.py
@@ -19,7 +19,7 @@ def normalize(image: np.ndarray) -> np.ndarray:
     source, dest = 0 if len(image.shape) == 3 else 1, -1
 
     # moveaxis for array broadcasting, and then back so its how pytorch expects it
-    return np.moveaxis((np.moveaxis(image, source, dest) - MEAN) * STD, dest, source)
+    return np.moveaxis((np.moveaxis(image, source, dest) - MEAN) / STD, dest, source)
 
 
 def denormalize(image: np.ndarray) -> np.ndarray:
@@ -29,12 +29,15 @@ def denormalize(image: np.ndarray) -> np.ndarray:
     # stack of images. If a stack, expected in (batch, channels, height, width)
     source, dest = 0 if len(image.shape) == 3 else 1, -1
 
-    image = np.moveaxis((np.moveaxis(image, source, dest) / STD) + MEAN, dest, source)
+    image = np.moveaxis((np.moveaxis(image, source, dest) * STD) + MEAN, dest, source)
     return (image * 255).astype(int)
 
 
-def make_masks(dataset_length: int, val_size: float = 0.1,
-               test_size: float = 0.1) -> Tuple[List[bool], List[bool], List[bool]]:
+def make_masks(
+    dataset_length: int,
+    val_size: float = 0.1,
+    test_size: float = 0.1
+    ) -> Tuple[List[bool], List[bool], List[bool]]:
     """Returns three boolean arrays of length `dataset_length`,
     representing the train set, validation set and test set. These
     arrays can be passed to `Dataset.add_mask` to yield the appropriate